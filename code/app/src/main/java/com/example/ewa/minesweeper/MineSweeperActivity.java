8529def050d9998990724c44070870b9a33817ce
elipinska <ewa.lipinska@hotmail.com>
2018-05-01 13:13:54 +0100

    Add menu with new game button

--- code/app/src/main/java/com/example/ewa/minesweeper/MineSweeperActivity.java
+++ code/app/src/main/java/com/example/ewa/minesweeper/MineSweeperActivity.java
@@ -2,7 +2,11 @@ package com.example.ewa.minesweeper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.GridView;
+import android.widget.Toast;
 
 public class MineSweeperActivity extends AppCompatActivity {
 
@@ -18,6 +22,29 @@ public class MineSweeperActivity extends AppCompatActivity {
         GridView gridview = findViewById(R.id.gameBoardGridView);
         gridview.setAdapter(boardAdapter);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.game_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //This will get called when the menu item gets clicked
+        if(item.getItemId() == R.id.new_game) {
+            Board board = new Board(20);
+            
+            BoardAdapter boardAdapter = new BoardAdapter(this, board);
+
+            GridView gridview = findViewById(R.id.gameBoardGridView);
+            gridview.setAdapter(boardAdapter);
+
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 
