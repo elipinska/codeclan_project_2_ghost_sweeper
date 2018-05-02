9c6c6be035d3a0ac9c02a5d96c4e2df7579e37c9
elipinska <ewa.lipinska@hotmail.com>
2018-05-02 16:46:22 +0100

    Add links to the About activity

--- code/app/src/main/java/com/example/ewa/ghostsweeper/AboutActivity.java
+++ code/app/src/main/java/com/example/ewa/ghostsweeper/AboutActivity.java
@@ -2,12 +2,23 @@ package com.example.ewa.ghostsweeper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity {
+
+    private TextView aboutInfoTextView;
+    private TextView creditsTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        aboutInfoTextView = findViewById(R.id.aboutInfoTextView);
+        creditsTextView = findViewById(R.id.creditsInfoTextView);
+
+        aboutInfoTextView.setMovementMethod(LinkMovementMethod.getInstance());
+        creditsTextView.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
